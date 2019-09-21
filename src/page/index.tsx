77bbb998acb37045c4ca176f9389e88700aff262
xiaohulu <idocument@qq.com>
2019-09-21 19:56:44 +0800

    因为基于函数的部件不支持 afterRender，所以改为基于类的部件

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,17 +1,15 @@
-import { create, tsx } from '@dojo/framework/core/vdom';
-import theme from '@dojo/framework/core/middleware/theme';
+import { tsx} from '@dojo/framework/core/vdom';
 import * as css from './index.m.css';
+import WidgetBase from '@dojo/framework/core/WidgetBase';
 
-interface PageProperties {
+export interface PageProperties {
     onLoad: () => void; // 一个页面只执行一次，当加载完之后重新渲染页面
 }
 
-const factory = create({ theme }).properties<PageProperties>();
-
-export default factory(function Page({ middleware: { theme }, properties }) {
-	const {  } = properties();
-    const {root} = theme.classes(css);
-    return (
-        <div classes={[root]}></div>
-    );
-});
+export default class Page extends WidgetBase<PageProperties> {
+    protected render() {
+        return (
+            <div key="page" classes={[css.root]}>{this.children}</div>
+        );
+    }
+}
