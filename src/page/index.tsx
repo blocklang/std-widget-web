7efcc9e187df5e06673f472794160bb63109814b
xiaohulu <idocument@qq.com>
2019-12-15 10:56:23 +0800

    将 onLoad 属性改为可选属性

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,19 +1,21 @@
-import { tsx} from '@dojo/framework/core/vdom';
-import * as css from './index.m.css';
-import WidgetBase from '@dojo/framework/core/WidgetBase';
+import { tsx } from "@dojo/framework/core/vdom";
+import * as css from "./index.m.css";
+import WidgetBase from "@dojo/framework/core/WidgetBase";
 
 export interface PageProperties {
-    onLoad: () => void; // 一个页面只执行一次，当加载完之后重新渲染页面
+	onLoad?: () => void; // 一个页面只执行一次，当加载完之后重新渲染页面
 }
 
 export default class Page extends WidgetBase<PageProperties> {
-    protected render() {
-        return (
-            <div key="page" classes={[this.getRootCssClass()]}>{this.children}</div>
-        );
-    }
+	protected render() {
+		return (
+			<div key="page" classes={[this.getRootCssClass()]}>
+				{this.children}
+			</div>
+		);
+	}
 
-    protected getRootCssClass(): string {
-        return css.root;
-    }
+	protected getRootCssClass(): string {
+		return css.root;
+	}
 }
