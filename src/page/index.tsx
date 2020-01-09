46fc5e97066d72f99d9b83d0f2361fb78d6df257
xiaohulu <idocument@qq.com>
2020-01-09 09:43:04 +0800

    将 Page 改为基于函数的部件 (#1)

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,21 +1,20 @@
-import { tsx } from "@dojo/framework/core/vdom";
+import { create, tsx } from "@dojo/framework/core/vdom";
 import * as css from "./index.m.css";
-import WidgetBase from "@dojo/framework/core/WidgetBase";
 
 export interface PageProperties {
-	onLoad?: () => void; // 一个页面只执行一次，当加载完之后重新渲染页面
+	// 一个页面只执行一次，当加载完之后重新渲染页面
+	onLoad?: () => void;
 }
 
-export default class Page extends WidgetBase<PageProperties> {
-	protected render() {
-		return (
-			<div key="page" classes={[this.getRootCssClass()]}>
-				{this.children}
-			</div>
-		);
-	}
+const factory = create().properties<PageProperties>();
 
-	protected getRootCssClass(): string {
-		return css.root;
-	}
-}
+export default factory(function Page({ properties, children }) {
+	const { onLoad } = properties();
+	onLoad && onLoad();
+
+	return (
+		<div key="root" classes={[css.root]}>
+			{children()}
+		</div>
+	);
+});
---
+++ tests/unit/page/index.tsx
@@ -0,0 +1,32 @@
+const { describe, it } = intern.getInterface("bdd");
+const { assert } = intern.getPlugin("chai");
+
+import assertionTemplate from "@dojo/framework/testing/assertionTemplate";
+import harness from "@dojo/framework/testing/harness";
+import { tsx } from "@dojo/framework/core/vdom";
+import Page from "../../../src/page";
+import * as css from "../../../src/page/index.m.css";
+import { stub } from "sinon";
+
+describe("Page", () => {
+	const baseAssertion = assertionTemplate(() => <div key="root" classes={[css.root]}></div>);
+
+	it("default properties", () => {
+		const h = harness(() => <Page />);
+		h.expect(baseAssertion);
+	});
+
+	it("onLoad event", () => {
+		const onLoadStub = stub();
+		const h = harness(() => <Page onLoad={onLoadStub} />);
+		h.expect(baseAssertion);
+		assert.isTrue(onLoadStub.calledOnce);
+	});
+
+	it("children", () => {
+		const childrenAssertion = baseAssertion.setChildren("@root", () => ["Hello"]);
+
+		const h = harness(() => <Page>Hello</Page>);
+		h.expect(childrenAssertion);
+	});
+});
